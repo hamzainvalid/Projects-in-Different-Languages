dc5c24decb5ddc32524e90f16101ef9c950d71d1
Muhammad Hamza Bin Qadir <82185151+hamzainvalid@users.noreply.github.com>
2022-05-20 19:08:05 +0200

    Update multiplicationTable.js

--- multiplicationTable.js
+++ multiplicationTable.js
@@ -1,14 +1,19 @@
-// program to generate a multiplication table
+// program to generate multiplication tables
 
 // take input from the user
-const number = parseInt(prompt('Enter an integer: '));
+const number = parseInt(prompt('Enter the number of tables: '));
 
 //creating a multiplication table
-for(let i = 1; i <= 10; i++) {
+for(let k = 1; k<=number; k++){
+  for(let i = 1; i <= 10; i++) {
 
     // multiply i with number
-    const result = i * number;
+    const result = i * k;
 
     // display the result
-    console.log(`${number} * ${i} = ${result}`);
+    console.log(`${k} * ${i} = ${result}`);
+    if(i == 10){
+      console.log('');
+    }
+}
 }
